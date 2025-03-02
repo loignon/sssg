c34a736d3ba269651f7cbc0891a2977b9df5e9c3
stev <steven.gibone@zaclys.net>
2025-03-02 08:33:42 +0100

    add HTMLNode and TextNode

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+        
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        val = ""
+        if self.props is not None:
+            for prop in self.props:
+                val = val + f' {prop}="{self.props[prop]}"'
+        return val
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode,TextType
+
+
+def main():
+    print(TextNode("hello", TextType.BOLD))
+
+if __name__ == "__main__":
+    main()

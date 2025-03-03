df26435d5d18823127e0f02a3c3f97cfb56042ed
stev <steven.gibone@zaclys.net>
2025-03-03 09:11:13 +0100

    add LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,3 +17,15 @@ class HTMLNode():
             for prop in self.props:
                 val = val + f' {prop}="{self.props[prop]}"'
         return val
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -31,3 +31,7 @@ class TestHTMLNode(unittest.TestCase):
         expected_str = "HTMLNode(a, None, None, {'href': 'https://ploum.net', 'target': '_blank'})"
         self.assertEqual(repr(node), expected_str)
 
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
